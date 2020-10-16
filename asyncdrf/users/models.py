d53d98daadcfd504fddb6227d101454f57cd818a
romax2000 <roman4ik2000@gmail.com>
2020-10-16 10:24:14 +0300

    models create

--- asyncdrf/users/models.py
+++ asyncdrf/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    code = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=128, unique=True)
+    actual_price = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
